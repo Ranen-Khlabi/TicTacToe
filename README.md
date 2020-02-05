1c5e9b8efb7c9881813eddd872040aa2d7d88690
RanenDev <github/RanenDev>
2020-02-05 17:06:16 +0300

    Modify

--- README.md
+++ README.md
@@ -1,13 +1,39 @@
-# TicTacToe
+# # Overview:
+
 # ![GA Logo](https://ga-dash.s3.amazonaws.com/production/assets/logo-9f88ae6c9c3871690e33280fcf557f33.png) 
-# Project #1: Tic Tac Toe
 
-## Overview:
 
-Used languges:
-- JavaScript
-- DOM
+List technologies used to build tic tac toe I used:
+- html
+- css 
+- js with DOM
 
-- Front End
-    - HTML
-    - CSS
+
+# user stories:
+
+player 1, by defult click on squre to add X
+
+player 2, by defult click on squre to add O
+
+player 2 will play with 'X'
+
+player can play again if he/she clicked on Replay button
+
+player can turn music if he/she clicked on (music on) button
+
+
+
+### develpment process
+
+* i wrote html and css code then js code
+
+* i defined a variable call player to player turn and switching between player 1 & 2 and two variables for character 'X' and 'O'
+
+* I used DOM for click event
+
+* I created methode call winnerpleyr to looking for winner in each time the user add on squre to find the winner
+
+
+# List unsolved problems which would be fixed in future iterations:
+* counting the winning in playing with computer case.
+* player choose the Mark "X" or "O" then start Game.
--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
-// Create function for Music
 var music = document.getElementById("music");
 var isPlaying = false;
 music.volume = 0.2;
+
+
+// Create function for Music play
 function togglePlay() {
   if (isPlaying) {
     music.pause()
@@ -9,15 +11,20 @@ function togglePlay() {
     music.play();
   }
 };
+
+
+//Take 'music' function if play ===> add 'on'
 music.onplaying = function() {
   isPlaying = true;
   document.getElementById("music-animation").classList.add('on')
 };
+//Take 'music' function if pause ===> remove 'on'
 music.onpause = function() {
   isPlaying = false;
   document.getElementById("music-animation").classList.remove('on')
 };
 
+//Create 'button' to swap between on & of
 var button = document.getElementById("toggle");
 button.addEventListener('click', function() {
   if (button.getAttribute("data-text-swap") == button.innerHTML) {
@@ -30,9 +37,14 @@ button.addEventListener('click', function() {
 
 
 
+//*************************************************************************//
+
+
+
 //Call ===> select all inside class ( cell )
 const cells = Array.from(document.querySelectorAll(".cell"));
-//Create Array for win
+
+//Create Array for winner
 const winner =[
 [1,2,3],
 [4,5,6],
@@ -44,17 +56,16 @@ const winner =[
 [3,6,9]
 ];
 
-//Declar Varible
+//Declar Varible for player
 let firstPlayer = [];
 let secondPlayer = [];
 let count = 0;
 
 
-
 //Start function
 function check(array){
   let finalResult = false;
-  //Create forLoop 
+  //Create for Loop 
   for(let item of winner){
     let res = item.every(val => array.indexOf(val) !== -1);
     if(res){
@@ -65,9 +76,14 @@ function check(array){
   return finalResult;
 }
 
-//Create function 
-function winnerpleyr(p){
 
+
+//*************************************************************************//
+
+
+
+//Create function for bottun if win, lose or tie 
+function winnerpleyr(p){
 //Create the HTML element ===> "div"
   const modal = document.createElement("div");
 //Create a Text Node with the specified text. 
@@ -75,7 +91,6 @@ function winnerpleyr(p){
   const replay = document.createElement("button");
 //returns
   modal.classList.add("winner");
-
   modal.appendChild(player);
   replay.appendChild(document.createTextNode("Replay"));
   // replay.setAttribute("onclick","rep();");
@@ -86,38 +101,63 @@ function winnerpleyr(p){
   document.body.appendChild(modal);
 }
 
+
+//*************************************************************************//
+
+
+
 //Create function 
 function tie(){
+//Add sound when click " X "
   let soundEiv = new Audio('Finger.mp3')
+
   if(this.classList == "cell"){
     count++;
     if(count%2 !== 0){
+//sound when " X " click
       soundEiv.play();
       this.classList.add("x");
       firstPlayer.push(Number(this.dataset.index));
-      if(check(firstPlayer)){        
+
+
+//***************************//
+
+
+    //check the first play " X " if win show the message 
+    if(check(firstPlayer)){        
         winnerpleyr("Congrats X Win :)");
+    //add removeEventListener when " X " is win
         cells.forEach(cell => cell.removeEventListener("click", tie));
         return true;
       }
     } else{
       this.classList.add("o");
       secondPlayer.push(Number(this.dataset.index));
-      if(check(secondPlayer)){
+
+    //check the first play " O " if win show the message 
+    if(check(secondPlayer)){
         winnerpleyr("Congrats O Win :)");
+    //add removeEventListener when " O " is win
         cells.forEach(cell => cell.removeEventListener("click", tie));
         return true;
-      }
+     }
     }
+
+    //message when tie
     if(count === 9){
       winnerpleyr("Tie :(");
     }
   }
 }
 
+
+//*************************************************************************//
+
+
+// function replay 
 function rep(){
   const win = document.querySelector(".winner");
-  // cells
+// 
   firstPlayer = [];
   secondPlayer = [];
   count = 0;
@@ -128,5 +168,5 @@ function rep(){
   });
 }
  
-
+//add loop and add addEventListener 
 cells.forEach(cell => cell.addEventListener("click", tie));
